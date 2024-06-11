24e628367f744c682c9deff28202133cf50caf9d
Fanta <96235087+Karma177@users.noreply.github.com>
2024-06-12 00:45:14 +0200

    ultimo fix del videoooooooo
    se chiudi la schermata però non la ripigli più....

--- src/main/java/it/davincifascetti/controllosocketudp/program/Video.java
+++ src/main/java/it/davincifascetti/controllosocketudp/program/Video.java
@@ -7,12 +7,12 @@ import me.friwi.tello4j.api.drone.TelloDrone;
 import me.friwi.tello4j.api.drone.WifiDroneFactory;
 import me.friwi.tello4j.api.video.VideoWindow;
 
-public class Video extends Component {
+public class Video extends Component implements Runnable {
     public static final int TIME_BEFORE_CLOSING_FRAME = 2000;
     private final static AtomicBoolean INIT = new AtomicBoolean();
     private TelloDrone listeningTo = null;
     private VideoWindow videoWindow = null;
-    private boolean alive = false;
+    private Thread thread = null;
 
     public Video(Ui ui) throws CommandException{
         this.setUi(ui);
@@ -20,23 +20,28 @@ public class Video extends Component {
 
     public void start(){
         if(Video.INIT.compareAndSet(false, true)){
-            try{
-                this.listeningTo = new WifiDroneFactory().build();
-                this.videoWindow = new VideoWindow();
-                this.listeningTo.connect();
-                this.listeningTo.addVideoListener(this.videoWindow);
-                this.listeningTo.setStreaming(true);
-                this.alive = true;
-                while (this.alive) ;
-            }catch(Exception e){
-                Video.INIT.set(false);
-                ((Terminal) this.getUi()).getCli().printError(e.getMessage());
-            }
+           this.thread = new Thread(this);
+           this.thread.start();
+        }
+    }
+
+    public void run(){
+        try{
+            this.listeningTo = new WifiDroneFactory().build();
+            this.videoWindow = new VideoWindow();
+            this.videoWindow.setSize(960,720);
+            this.listeningTo.connect();
+            this.listeningTo.addVideoListener(this.videoWindow);
+            this.listeningTo.setStreaming(true);
+            while (this.listeningTo.isStreaming())  ;
+        }catch(Exception e){
+            Video.INIT.set(false);
+            ((Terminal) this.getUi()).getCli().printError(e.getMessage());
         }
     }
 
     public void destroy(){
-        this.alive = false;
+        this.thread.interrupt();
         super.destroy();
         this.listeningTo.removeVideoListener(this.videoWindow);
         this.videoWindow.dispose();
